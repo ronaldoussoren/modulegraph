87159b2d3fdf82db926d7fe14d66580e3f7d91f9
Ronald Oussoren <ronaldoussoren@mac.com>
2010-07-27 18:49:24 +0000

    Add maintainer info

--- setup.py
+++ setup.py
@@ -19,6 +19,23 @@ by bytecode analysis for import statements.
 modulegraph uses similar methods to modulefinder from the standard library,
 but uses a more flexible internal representation, has more extensive 
 knowledge of special cases, and is extensible.
+
+NEWS
+====
+
+0.8
+---
+
+This is a minor feature release
+
+Features:
+
+- Initial support for Python 3.x
+
+- It is now possible to run the test suite
+  using ``python setup.py test``.
+
+  (The actual test suite is still fairly minimal though)
 """
 
 CLASSIFIERS = filter(None, map(str.strip,
@@ -26,6 +43,7 @@ CLASSIFIERS = filter(None, map(str.strip,
 Intended Audience :: Developers
 License :: OSI Approved :: MIT License
 Programming Language :: Python
+Programming Language :: Python :: 2
 Programming Language :: Python :: 3
 Topic :: Software Development :: Libraries :: Python Modules
 Topic :: Software Development :: Build Tools
@@ -46,6 +64,8 @@ setup(
     classifiers=CLASSIFIERS,
     author="Bob Ippolito",
     author_email="bob@redivi.com",
+    maintainer="Ronald Oussoren",
+    maintainer_email="ronaldoussoren@mac.com",
     url="http://undefined.org/python/#modulegraph",
     license="MIT License",
     packages=['modulegraph'],
