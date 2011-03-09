7f262aa98ba1bdd512777925a23120c50eea80aa
Ronald Oussoren <ronaldoussoren@mac.com>
2011-03-09 14:30:36 -0500

    Use unittest API

--- modulegraph_tests/test_util.py
+++ modulegraph_tests/test_util.py
@@ -2,21 +2,51 @@ import unittest
 import encodings
 import encodings.aliases
 from modulegraph import util
+from modulegraph._compat import B
+import sys
+
+try:
+    from io import BytesIO
+except ImportError:
+    from cStringIO import StringIO as BytesIO
 
 class TestUtil (unittest.TestCase):
     def test_imp_find_module(self):
         fn = util.imp_find_module('encodings.aliases')[1]
-        assert encodings.aliases.__file__.startswith(fn)
+        self.assertTrue(encodings.aliases.__file__.startswith(fn))
 
     def test_imp_walk(self):
         imps = list(util.imp_walk('encodings.aliases'))
-        assert len(imps) == 2
+        self.assertEqual(len(imps), 2)
 
-        assert imps[0][0] == 'encodings'
-        assert encodings.__file__.startswith(imps[0][1][1])
+        self.assertEqual(imps[0][0], 'encodings')
+        self.assertTrue(encodings.__file__.startswith(imps[0][1][1]))
 
-        assert imps[1][0] == 'aliases'
-        assert encodings.aliases.__file__.startswith(imps[1][1][1])
+        self.assertEqual(imps[1][0], 'aliases')
+        self.assertTrue(encodings.aliases.__file__.startswith(imps[1][1][1]))
+
+    def test_guess_encoding(self):
+        fp = BytesIO(B("# coding: utf-8"))
+        self.assertEqual(util.guess_encoding(fp), "utf-8")
+
+        fp = BytesIO(B("\n# coding: utf-8"))
+        self.assertEqual(util.guess_encoding(fp), "utf-8")
+
+        fp = BytesIO(B("# coding: latin-1"))
+        self.assertEqual(util.guess_encoding(fp), "latin-1")
+
+        fp = BytesIO(B("\n# coding: latin-1"))
+        self.assertEqual(util.guess_encoding(fp), "latin-1")
+
+        fp = BytesIO(B("#!/usr/bin/env/python\n# vim: set fileencoding=latin-1 :"))
+        self.assertEqual(util.guess_encoding(fp), "latin-1")
+
+        fp = BytesIO(B("\n\n\n# coding: latin-1"))
+        if sys.version_info[0] == 2:
+            self.assertEqual(util.guess_encoding(fp), "ascii")
+        else:
+            self.assertEqual(util.guess_encoding(fp), "utf-8")
+            
 
 if __name__ == "__main__":
     unittest.main()
